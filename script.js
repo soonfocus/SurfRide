ae5f834c813fa6055575add646276e8c7ebd77fa
soonfocus <75900253+soonfocus@users.noreply.github.com>
2024-07-14 12:47:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
 
+/* Timer */
+let timerInterval;
+let totalSeconds = 0;
+
+const updateTimerDisplay = () => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    document.getElementById('timer').innerText = 
+        String(minutes).padStart(2, '0') + ':' + String(seconds).padStart(2, '0');
+}
+
+const startTimer= () => {
+    if (!timerInterval) {
+        timerInterval = setInterval(() => {
+            totalSeconds++;
+            updateTimerDisplay();
+        }, 1000);
+    }
+}
+
+startTimer()
+
+
+/* Create object */
+const createObject = (className, id) => {
+  const newElement = document.createElement('div');
+  newElement.classList.add(className);
+  newElement.id = id
+  document.body.appendChild(newElement);
+}
+
+
+
+/* # Ride Vertical #*/
+/* Tube ride */
+const {left: tubeLeft, right: tubeRight} = document.getElementById('tube').getBoundingClientRect()
+console.log('TUBE',tubeLeft,tubeRight);
+
+
+
+
+
 /* Detect position */
 const handleOrientation = (data) => {
   const {alpha, beta, gamma} = data
 console.log(alpha, gamma, beta);
 
 const surfer = document.getElementById('surfer')
-
 surfer.style.transform=`translateX(${(gamma ?? 0) * 10}px)`;
+
+const {left: surferLeft, right: surferRight} = surfer.getBoundingClientRect()
+
+console.log('SURFER',surferLeft, surferRight);
+
+
 }
 
 
